3beab711bda9b749276803ece4eb10a3d02c6536
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2022-03-31 23:10:08 +0100

    basic page setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,31 @@
 import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
-import './App.css';
 
+import useStyles from './styles';
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 import memories from './images/memories.png'
 
 function App() {
+  const classes = useStyles()
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inhert">
-        <Typography variant="h2" align="center">Hey</Typography>
-        <img src={memories} alt="memories" heigth="60" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Hey</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
       </AppBar>
 
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3} >
+            <Grid item xs={12} sm={7} >
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4} >
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 }
